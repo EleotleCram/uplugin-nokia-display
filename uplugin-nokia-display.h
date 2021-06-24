790a95e1eb86e38168b55d852ae139f647a29814
Marcel Toele <EleotleCram@gmail.com>
2021-06-24 14:29:02 +0200

    uplugin-nokia-display.h: Added BL (Backlight) pin and inverted mode args

--- uplugin-nokia-display.h
+++ uplugin-nokia-display.h
@@ -1,21 +1,28 @@
 
 #pragma once
 
-#include <uPlugin.h>
 #include <Adafruit_PCD8544.h>
+#include <uPlugin.h>
 
 class NokiaDisplay : public Adafruit_PCD8544, public uPlugin {
+  bool inverted;
+  uint8_t bl;
 
 protected:
   void NokiaDisplay::drawFastText(uint8_t x, uint8_t y, const char *text);
 
 public:
 #ifdef USE_HARDWARE_SPI
-                                                                                 // Not specifying clk and mosi
-                                                                                 // enables hardware SPI mode:
-  NokiaDisplay(uint8_t clk, uint8_t mosi, uint8_t dc, uint8_t ce, uint8_t rst) : Adafruit_PCD8544(dc, ce, rst) {
+  // Not specifying clk and mosi
+  // enables hardware SPI mode:
+  NokiaDisplay(uint8_t clk, uint8_t mosi, uint8_t dc, uint8_t ce, uint8_t rst,
+               uint8_t bl, bool bl_inverted)
+      : Adafruit_PCD8544(dc, ce, rst), bl(bl), inverted(bl_inverted) {
 #else
-  NokiaDisplay(uint8_t clk, uint8_t mosi, uint8_t dc, uint8_t ce, uint8_t rst) : Adafruit_PCD8544(clk, mosi, dc, ce, rst) {
+  NokiaDisplay(uint8_t clk, uint8_t mosi, uint8_t dc, uint8_t ce, uint8_t rst,
+               uint8_t bl, bool bl_inverted)
+      : Adafruit_PCD8544(clk, mosi, dc, ce, rst), bl(bl),
+        inverted(bl_inverted) {
 #endif
     //
   }
